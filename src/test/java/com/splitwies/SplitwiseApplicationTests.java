6506b57155c17d4d33df0f1a106c32f828fcefcc
Surojit Santra <surojitsantra97@gmail.com>
2023-10-01 13:32:40 +0530

    Project Setup

---
+++ src/test/java/com/splitwies/SplitwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitwies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
